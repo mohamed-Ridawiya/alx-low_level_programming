d89c7f35ad5c5e7a6d1b37e2a1acf523a0b3ece7
mohamed-Ridawiya <mohamed.ridawiya@gmail.com>
2023-05-24 04:04:12 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - val of int
+ * @n: Description of abs val
+ *
+ * Description:abs val
+ * Return: Description of the returned value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+	}
+	return (n);
+}
